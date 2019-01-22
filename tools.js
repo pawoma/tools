e458e10410a41ef32d2faeb07a967ff54fc98344
pcli <294805740@qq.com>
2019-01-22 09:37:29 +0800

    update code

---
+++ tools.js
@@ -0,0 +1,24 @@
+
+// 深拷贝
+function cloneDeep(source){
+   if(typeof source !== 'object' || source === null){
+       return source
+    }
+    
+    var isArray = Object.prototype.toString.call(source) === '[object Array]'
+    var target = isArray ?[]:{}
+    
+    for(var key in source){
+        if(Object.prototype.hasOwnProperty.call(source,key)){
+            if(typeof source[key] === 'object' && source[key] !== null){
+                target[key] = cloneDeep(source[key])
+            }else{
+                target[key] = source[key] 
+            }
+            
+        }
+    }
+
+    return target
+}
+
